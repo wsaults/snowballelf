1b2f5fc0a545b480c2b27802679adefc4d990ea4
Will Saults <will@saults.io>
2019-12-02 09:03:31 -0600

    December 2nd update

--- src/App.js
+++ src/App.js
@@ -3,28 +3,31 @@ import elf from './images/elf.png';
 import decor from './images/decor.png';
 import dec1a from './images/dec1a.jpg';
 import dec1b from './images/dec1b.jpg';
+import dec2a from './images/dec2a.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Daily Snowball the Elf Update!</h1>
+        <h1>Snowball the Elf</h1>
         <img className="decor" src={decor} alt="decor" />
       </header>
       <div className="day">
-        <h2>December 1st</h2>
+        <h2>December 1st 2019</h2>
         <div className="row">
           <img src={dec1a} alt="December 1st Picture 1" />
           <img src={dec1b} alt="December 1st Picture 2" />
         </div>
       </div>
       <div className="day">
-        <h2>December 2nd</h2>
-        <p>Coming soon!</p>
+        <h2>December 2nd 2019</h2>
+        <div className="row">
+          <img src={dec2a} alt="December 2nd Picture 1" />
+        </div>
       </div>
       <div className="day">
-        <h2>December 3rd</h2>
+        <h2>December 3rd 2019</h2>
         <p>Coming soon!</p>
       </div>
       <footer>
