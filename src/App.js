01cab44c2cd7e4afb38d11734e8122d952e39e87
Will Saults <will@saults.io>
2019-12-04 15:50:47 -0600

    December 4th

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import dec1a from './images/dec1a.jpg';
 import dec1b from './images/dec1b.jpg';
 import dec2a from './images/dec2a.jpg';
 import dec3a from './images/dec3a.jpg';
+import dec4a from './images/dec4a.jpg';
 import './App.css';
 
 function App() {
@@ -22,24 +23,31 @@ function App() {
       </div>
 
       <div className="day">
+        <h2>December 4th 2019</h2>
+        <div className="row">
+          <img src={dec4a} alt="December 4th" />
+        </div>
+        <p className="subtext">Don't forget to brush!</p>
+      </div>
+      <div className="day">
         <h2>December 3rd 2019</h2>
         <div className="row">
-          <img src={dec3a} alt="December 3rd Picture 1" />
+          <img src={dec3a} alt="December 3rd" />
         </div>
         <p className="subtext">Napping in Isaac's bed</p>
       </div>
       <div className="day">
         <h2>December 2nd 2019</h2>
         <div className="row">
-          <img src={dec2a} alt="December 2nd Picture 1" />
+          <img src={dec2a} alt="December 2nd" />
         </div>
         <p className="subtext">Diving into Zander's stocking</p>
       </div>
       <div className="day">
         <h2>December 1st 2019</h2>
         <div className="row">
-          <img src={dec1a} alt="December 1st Picture 1" />
-          <img src={dec1b} alt="December 1st Picture 2" />
+          <img src={dec1a} alt="December 1st" />
+          <img src={dec1b} alt="December 1st" />
         </div>
         <p className="subtext">Snowball has arrived!</p>
       </div>
