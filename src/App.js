c0cb96b93470d4764d4f57d5b04662b2b821e58c
Will Saults <will@saults.io>
2019-12-22 11:53:54 -0600

    Added missing days

--- src/App.js
+++ src/App.js
@@ -10,6 +10,20 @@ import dec5a from './images/dec5a.jpg';
 import dec5b from './images/dec5b.jpg';
 import dec6a from './images/dec6a.jpg';
 import dec7a from './images/dec7a.jpg';
+import dec8 from './images/dec8.jpg';
+import dec9 from './images/dec9.jpg';
+import dec11 from './images/dec11.jpg';
+import dec12 from './images/dec12.jpg';
+import dec13 from './images/dec13.jpg';
+import dec14 from './images/dec14.jpg';
+import dec15 from './images/dec15.jpg';
+import dec16 from './images/dec16.jpg';
+import dec17 from './images/dec17.jpg';
+import dec18 from './images/dec18.jpg';
+import dec19 from './images/dec19.jpg';
+import dec20 from './images/dec20.jpg';
+import dec21 from './images/dec21.jpg';
+import dec22 from './images/dec22.jpg';
 import './App.css';
 
 function App() {
@@ -26,6 +40,110 @@ function App() {
         </p>
       </div>
 
+      <div className="day">
+        <h2>December 22nd 2019</h2>
+        <div className="row">
+          <img src={dec22} alt="December 22nd" />
+        </div>
+        <p className="subtext">Welcome back! Snowball left a treat :-)</p>
+      </div>
+      <div className="day">
+        <h2>December 21st 2019</h2>
+        <div className="row">
+          <img src={dec21} alt="December 21st" />
+        </div>
+        <p className="subtext">Am I doing this right?</p>
+      </div>
+      <div className="day">
+        <h2>December 20th 2019</h2>
+        <div className="row">
+          <img src={dec20} alt="December 20th" />
+        </div>
+        <p className="subtext">I'll watch little man for ya!</p>
+      </div>
+      <div className="day">
+        <h2>December 19th 2019</h2>
+        <div className="row">
+          <img src={dec19} alt="December 19th" />
+        </div>
+        <p className="subtext">Who's ready for Christmas?</p>
+      </div>
+      <div className="day">
+        <h2>December 18th 2019</h2>
+        <div className="row">
+          <img src={dec18} alt="December 18th" />
+        </div>
+        <p className="subtext">Ahhh, nothing like a hot cocoa bath.</p>
+      </div>
+      <div className="day">
+        <h2>December 17th 2019</h2>
+        <div className="row">
+          <img src={dec17} alt="December 17th" />
+        </div>
+        <p className="subtext">Ok, Google. Play, "We are Santa's Elves".</p>
+      </div>
+      <div className="day">
+        <h2>December 16th 2019</h2>
+        <div className="row">
+          <img src={dec16} alt="December 16th" />
+        </div>
+        <p className="subtext">When's dinner?</p>
+      </div>
+      <div className="day">
+        <h2>December 15th 2019</h2>
+        <div className="row">
+          <img src={dec15} alt="December 15th" />
+        </div>
+        <p className="subtext">Wishing the big boys an awesome week at Mom's!</p>
+      </div>
+      <div className="day">
+        <h2>December 14th 2019</h2>
+        <div className="row">
+          <img src={dec14} alt="December 14th" />
+        </div>
+        <p className="subtext">I Guess that's why they call him Elf on a shelf!</p>
+      </div>
+      <div className="day">
+        <h2>December 13th 2019</h2>
+        <div className="row">
+          <img src={dec13} alt="December 13th" />
+        </div>
+        <p className="subtext">Sneaky elf!</p>
+      </div>
+      <div className="day">
+        <h2>December 12th 2019</h2>
+        <div className="row">
+          <img src={dec12} alt="December 12th" />
+        </div>
+        <p className="subtext">What does an Elf learn in school?</p>
+      </div>
+      <div className="day">
+        <h2>December 11th 2019</h2>
+        <div className="row">
+          <img src={dec11} alt="December 11th" />
+        </div>
+        <p className="subtext">Chilling with the big dudes.</p>
+      </div>
+      <div className="day">
+        <h2>December 10th 2019</h2>
+        <div className="row">
+        </div>
+        <p className="subtext">Oops, we'll get back to you on this one...</p>
+      </div>
+      <div className="day">
+        <h2>December 9th 2019</h2>
+        <div className="row">
+          <img src={dec9} alt="December 9th" />
+        </div>
+        <p className="subtext">Having fun at candy bowling!</p>
+      </div>
+      <div className="day">
+        <h2>December 8th 2019</h2>
+        <div className="row">
+          <img src={dec8} alt="December 8th" />
+        </div>
+        <p className="subtext">Look who's hangn' around! Thanks for the PJs :^D</p>
+      </div>
       <div className="day">
         <h2>December 7th 2019</h2>
         <div className="row">
